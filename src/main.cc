ba495297dac9d4d36efa8fc004e5ae2a70f032c3
yuecheng.pei <793499453@qq.com>
2020-09-30 10:15:11 +0800

    Complete Initiation task

--- src/main.cc
+++ src/main.cc
@@ -3,6 +3,7 @@
 #include <cdcf/logger.h>
 #include "blockable_count_actor.h"
 #include "count_down_latch.h"
+#include "mini_actor.h"
 
 std::vector<std::string> split(const std::string &input,
                                char delim) {
@@ -48,6 +49,60 @@ void handleEnqueueing(caf::actor_system &system, std::vector<std::string> &args)
     std::cout << "\t" << (n * 1000'000'000L / spent_time) << " ops/s" << std::endl;
 }
 
+void handleDequeueing(caf::actor_system &system, std::vector<std::string> &args) {
+    int n = std::atoi(args[1].c_str());
+    CountDownLatch start_latch(1);
+    CountDownLatch finish_latch(1);
+    auto actor = system.spawn(blockableCountActorFun, &start_latch, &finish_latch, n);
+
+    EmptyMessage empty_message;
+    for (int i = 0; i < n; i++) {
+        caf::anon_send(actor, empty_message);
+    }
+
+//  std::cout << "finish send message" << std::endl;
+    auto start = std::chrono::high_resolution_clock::now();
+    start_latch.countDown();
+    finish_latch.await();
+    auto spent_time = std::chrono::duration_cast<std::chrono::nanoseconds>(
+            std::chrono::high_resolution_clock::now() - start).count();
+
+    std::cout << "Dequeueing:" << std::endl;
+    std::cout << "\t" << n << " ops" << std::endl;
+    std::cout << "\t" << spent_time << " ns" << std::endl;
+    std::cout << "\t" << (n * 1000'000'000L / spent_time) << " ops/s" << std::endl;
+}
+
+void handleInitiation(caf::actor_system &system, std::vector<std::string> &args) {
+    int n = std::atoi(args[1].c_str());
+
+    std::vector<caf::actor> actor_list;
+    auto start = std::chrono::high_resolution_clock::now();
+    for(int i=0; i<n; i++) {
+        auto actor = system.spawn(miniFun, n);
+        actor_list.push_back(actor);
+    }
+    auto spent_time = std::chrono::duration_cast<std::chrono::nanoseconds>(
+            std::chrono::high_resolution_clock::now() - start).count();
+
+
+    for (auto actor : actor_list) {
+        // std::cout << "actor id: " << actor->id() << std::endl;
+        // std::cout << "actor address: " << caf::to_string(actor->address()) << std::endl;
+        caf::actor_cast<caf::event_based_actor*>(actor)->quit();
+    }
+
+    std::cout << "Initiation:" << std::endl;
+    std::cout << "\t" << n << " ops" << std::endl;
+    std::cout << "\t" << spent_time << " ns" << std::endl;
+    std::cout << "\t" << (n * 1000'000'000L / spent_time) << " ops/s" << std::endl;
+}
+
+void handleSingleProducerSending(caf::actor_system &system, std::vector<std::string> &args) {
+    int n = std::atoi(args[1].c_str());
+
+}
+
 [[noreturn]] void caf_main(caf::actor_system &system, const cdcf::actor_system::Config &config) {
     cdcf::Logger::Init(config);
 
@@ -80,6 +135,22 @@ void handleEnqueueing(caf::actor_system &system, std::vector<std::string> &args)
             handleEnqueueing(system, args);
             continue;
         }
+
+        if (command == "dequeueing") {
+            handleDequeueing(system, args);
+            continue;
+        }
+
+        if (command == "initiation") {
+            handleInitiation(system, args);
+            continue;
+        }
+
+        if (command == "single-producer-sending") {
+            handleSingleProducerSending(system, args);
+            continue;
+        }
+
     }
 }
 
---
+++ src/mini_actor.h
@@ -0,0 +1,13 @@
+//
+// Created by Yuecheng Pei on 2020/9/29.
+//
+
+#ifndef CDCF_PERFORMANCE_MEASURING_MINI_ACTOR_H
+#define CDCF_PERFORMANCE_MEASURING_MINI_ACTOR_H
+#include <caf/all.hpp>
+
+caf::behavior miniFun(caf::event_based_actor* self, int n) {
+    return {};
+}
+
+#endif //CDCF_PERFORMANCE_MEASURING_MINI_ACTOR_H
